360a203c73e84f2179ed4ae92e4eab365fb1e025
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2022-04-03 00:20:07 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const linksSocialMedia = {
+    github: "aristidesquetajr",
+    youtube: "jackelinygracielly",
+    facebook: "aristidesqueta",
+    instagram: "aristidesquetajr",
+    twitter: "jackelinytec"
+}
+
+function changeSocialMediaLinks() {
+    /* userName.textContent = "Marcelo" */
+    for(let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+    }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+    const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        userName.textContent = data.name
+        userBio.textContent = data.bio
+        userLink.href = data.html_url
+        userImage.src = data.avatar_url
+        userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
---
+++ notas.md
@@ -0,0 +1,9 @@
+URL
+lorempixes.com/width/height/people
+
+Para pegar imagens aleatorias
+
+pesquisa no google > clippy css ou a url(https//bennettfeely.com/clippy)
+    - Serve para alterar a forma das imagens apartir do css
+
+Clip-path: 
